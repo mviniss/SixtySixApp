3713e3ca75f7d2390577ae79d20e965958a97add
mviniss <m_vinicius.ss@icloud.com>
2022-01-12 19:13:58 -0300

    1st commit
    Criação do projeto

---
+++ SixtySixApp/SixtySixApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SixtySixApp
+//
+//  Created by Marcus Vinicius Silva de Sousa on 12/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SixtySixApp/SixtySixApp/SixtySixAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixtySixAppApp.swift
+//  SixtySixApp
+//
+//  Created by Marcus Vinicius Silva de Sousa on 12/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SixtySixAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
