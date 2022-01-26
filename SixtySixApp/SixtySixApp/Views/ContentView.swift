12276b336715e2a36c3f7ee8ba5a4e56315a9dd9
mviniss <m_vinicius.ss@icloud.com>
2022-01-26 20:54:38 -0300

    Onboarding + Start Button
    Finalização do Onboarding e Confecção do Start Button

--- SixtySixApp/SixtySixApp/Views/ContentView.swift
+++ SixtySixApp/SixtySixApp/Views/ContentView.swift
@@ -1,7 +1,9 @@
 import SwiftUI
 
+//HomeScreen
+
 struct ContentView: View {
-    @State var OnboardingAppears: Bool = true
+    @AppStorage("onboardingAppears") var onboardingAppears: Bool = true
     var body: some View {
         NavigationView {
             VStack {
@@ -9,8 +11,8 @@ struct ContentView: View {
             }
             .navigationTitle("Meus hábitos")
         }
-        .fullScreenCover(isPresented: $OnboardingAppears, content: {
-            OnboardingView()
+        .fullScreenCover(isPresented: $onboardingAppears, content: {
+            OnboardingView(onboardingAppears: $onboardingAppears)
         })
     }
 }
@@ -18,17 +20,12 @@ struct ContentView: View {
 //Onboarding
 
 struct OnboardingView: View {
+    @Binding var onboardingAppears: Bool
     var body: some View{
         TabView {
-            VStack {
-                PageView(message: "Segundo um estudo da professora Jane Wardle, da University College of London, o cérebro humano leva, em média, 66 dias para se acostumar a fazer algo novo.", imageName: "seeding")
-            }
-            VStack {
-                PageView(message: "Com o 66 dias, basta criar um novo hábito que você quer construir, escolher o dia que você quer começar (por que não hoje mesmo?) e escolher a hora do dia para ser lembrado!", imageName: "workout")
-            }
-            VStack {
-                PageView(message: "Uma dica: que tal identificar um hábito que você se identifica, que te dê prazer ou que você sabe que é para o seu crescimenhhto profissional? É mais fácil seguir em frente quando temos um propósito!", imageName: "reading")
-            }
+            PageView(message: "Segundo um estudo da professora Jane Wardle, da University College of London, o cérebro humano leva, em média, 66 dias para se acostumar a fazer algo novo.", imageName: "seeding", startButton: false, onboardingAppears: $onboardingAppears)
+            PageView(message: "Com o 66 dias, basta criar um novo hábito que você quer construir, escolher o dia que você quer começar (por que não hoje mesmo?) e escolher a hora do dia para ser lembrado!", imageName: "workout", startButton: false, onboardingAppears: $onboardingAppears)
+            PageView(message: "Uma dica: que tal começar um hábito que você se identifica, que te dê prazer ou que você sabe que é para o seu crescimento profissional? É mais fácil seguir em frente quando temos um propósito!", imageName: "reading", startButton: true, onboardingAppears: $onboardingAppears)
             }
         .tabViewStyle(PageTabViewStyle())
     }
@@ -37,7 +34,8 @@ struct OnboardingView: View {
 struct PageView: View{
     let message: String
     let imageName: String
-    
+    let startButton: Bool
+    @Binding var onboardingAppears: Bool
     var body: some View{
         VStack{
             Image(imageName)
@@ -47,6 +45,21 @@ struct PageView: View{
             Text(message)
                 .font(.system(size: 17))
                 .multilineTextAlignment(.leading)
+                .padding()
+            
+            if startButton{
+                Button(action: {
+                    onboardingAppears.toggle()
+                }, label: {
+                    Text("Começar")
+                        .bold()
+                        .font(.system(size: 17))
+                        .frame(width: 122, height: 44, alignment: .center)
+                        .foregroundColor(Color.white)
+                        .background(Color.purple)
+                        .cornerRadius(5)
+                })
+            }
         }
     }
 }
