68e2d8626f4de720479c8d967ee61afa3fff3f03
mviniss <m_vinicius.ss@icloud.com>
2022-01-26 23:17:15 -0300

    Início do calendário
    Início do calendário

--- SixtySixApp/SixtySixApp/Views/ContentView.swift
+++ SixtySixApp/SixtySixApp/Views/ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 //HomeScreen
 
 struct ContentView: View {
+    //@AppStorage é o que assegura que o Onboarding será chamado apenas uma única vez. Uma vez que o Botão "Começar" é pressionado, a função toggle na variável onboardingAppears, dentro da declaração Button presente no arquivo onboarding, armazena que o onboarding foi finalizado e ele não é mais chamado quando o usuário começa a usar o aplicativo
     @AppStorage("onboardingAppears") var onboardingAppears: Bool = true
     var body: some View {
         NavigationView {
